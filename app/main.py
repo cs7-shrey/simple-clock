e845b03ad6674d512316b97c491e5fe725a0e7b1
shrey <shrey@kali>
2024-10-15 13:13:47 +0530

    adding a websocket

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, WebSocket, WebSocketDisconnect
+import random
+import asyncio
 from fastapi.middleware.cors import CORSMiddleware
 import json
 
@@ -29,3 +31,17 @@ def get_companies():
     with open("./app/snp500.json", "r") as file:
         companies = json.load(file)
     return {"companies": companies}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            # Generate a random number between 1 and 10
+            random_number = random.randint(1, 10)
+            # Send the random number to the WebSocket client
+            await websocket.send_text(str(random_number))
+            # Wait for 2 seconds before sending the next number
+            await asyncio.sleep(2)
+    except WebSocketDisconnect:
+        print("Client disconnected")
